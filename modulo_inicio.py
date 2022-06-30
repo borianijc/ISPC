f2a6967038da99bc686ca99fecd6e022271cee45
borianijc <borianijc@gmail.com>
2022-06-29 21:26:17 -0300

    29/06/2022

--- modulo_inicio.py
+++ modulo_inicio.py
@@ -1,7 +1,15 @@
+from cProfile import label
 from tkinter import *
 
 ventana_inicio = Tk()
+
 ventana_inicio.title("INICIO")
+ventana_inicio.iconbitmap("Casa\library.ico")
 ventana_inicio.config(height=1500,width=1500)
 
+fondo_inicio=PhotoImage("Casa\library_1767.png")
+label_inicio= Label(ventana_inicio, image=fondo_inicio)
+label_inicio.grid(sticky=W)
+
 ventana_inicio.mainloop()
+ 
